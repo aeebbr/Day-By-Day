2ef3cf805dbd51a5dea510e27ea0c6511c0255c1
희진 <109948739+lmghee@users.noreply.github.com>
2023-06-25 05:00:09 +0900

    15683번 문제 주석 추가

--- DBD_heejin/2023_06/Day_07/15683.java
+++ DBD_heejin/2023_06/Day_07/15683.java
@@ -1,36 +1,40 @@
 // [BOJ] 15683. 감시
-// 풀이 시간: 00 분
-// 실행 시간: 0 ms
-// 메모리: 0 KB
+// 풀이 시간: ?? 분 (실험 좀 많이 함)
+// 실행 시간: 236 ms
+// 메모리: 53968 KB
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N, M, Ans;
-    public static int[][] map;
 
+    static class Camera {
+        int y, x, t;
+
+        Camera(int y, int x, int t) {
+            this.y = y;
+            this.x = x;
+            this.t = t;
+        }
+    }
+
+    static int N, M, Ans;
+    static int[][] map;
     // 오, 아, 왼, 위
     public static int[] dy = {0, 1, 0, -1};
     public static int[] dx = {1, 0, -1, 0};
+    
     public static int[][][] cctv = { {{}},
             {{0}, {1}, {2}, {3}},
             {{0, 2}, {1, 3}},
             {{0, 1}, {1, 2}, {2, 3}, {0, 3}},
             {{0, 1, 2}, {1, 2, 3}, {0, 2, 3} ,{0, 1, 3}},
             {{0, 1, 2, 3}}};
-    public static class Camera{
-        int r, c, t;
-        Camera(int r,int c, int t){
-            this.r=r;
-            this.c=c;
-            this.t=t;
-        }
-    }
-    public static ArrayList<Camera> cams;
+
+    static ArrayList<Camera> cams = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -40,13 +44,13 @@ public class Main {
         map= new int[N][M];
         cams = new ArrayList<>();
 
-        for (int r = 0; r < N; r++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < M; c++) {
-                map[r][c]=Integer.parseInt(st.nextToken());
-                if(map[r][c] >= 1 && map[r][c] <= 5) {
-                    cams.add(new Camera(r, c, map[r][c]));
-                } else if (map[r][c] == 0) {
+            for (int j = 0; j < M; j++) {
+                map[i][j]=Integer.parseInt(st.nextToken());
+                if(map[i][j] >= 1 && map[i][j] <= 5) {
+                    cams.add(new Camera(i, j, map[i][j]));
+                } else if (map[i][j] == 0) {
                     Ans++;
                 }
             }
@@ -55,53 +59,43 @@ public class Main {
         System.out.println(Ans);
     }
 
-    private static void solving(int idx, int[][] map) {
-        //basis part
-        // 모든 카메라로 지도를 검색했다면
+    public static void solving(int idx, int[][] map) {
         if(idx == cams.size()) {
-            // 지도의 사각지대를 구한다
+
             int cnt = 0;
-            //print(map);
-            for (int r = 0; r < N; r++) {
-                for (int c = 0; c < M; c++) {
-                    if(map[r][c] == 0) cnt++;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(map[i][j] == 0) cnt++;
                 }
             }
             Ans = Math.min(Ans, cnt);
             return;
         }
 
-        //inductive part
         Camera cam = cams.get(idx);
-        int r = cam.r;
-        int c = cam.c;
 
-        for (int i = 0; i < cctv[cam.t].length; i++) {
+        for(int i = 0; i < cctv[cam.t].length; i++) {
             int[][] tmap = new int[N][M];
-            for(int k = 0; k < N; ++k) {
-//                System.arraycopy(map[k], 0, tmap[k], 0, N);
-                tmap[k] = map[k].clone();
+            for(int k = 0; k < N; k++) {
+                System.arraycopy(map[k], 0, tmap[k], 0, M);
             }
 
-            for (int d = 0; d < cctv[cam.t][i].length; d++) {
-                int dir = cctv[cam.t][i][d];
-
-                int dr = r + dy[dir];
-                int dc = c + dx[dir];
+            for(int d = 0; d < cctv[cam.t][i].length; d++) {
+                int di = cam.y;
+                int dj = cam.x;
+                int nd = cctv[cam.t][i][d];
 
                 while(true) {
+                    di += dy[nd];
+                    dj += dx[nd];
 
-                    if (dr < 0 || dr >= N || dc < 0 || dc >= M) break;
-                    if (tmap[dr][dc] == 6) break;
+                    if (di < 0 || di >= N || dj < 0 || dj >= M) break;
+                    if (tmap[di][dj] == 6) break;
 
-                    tmap[dr][dc] = 9;
-
-                    dr += dy[dir];
-                    dc += dx[dir];
+                    tmap[di][dj] = 9;
                 }
-                solving(idx + 1, tmap);
             }
+            solving(idx+1, tmap);
         }
     }
 }
-
