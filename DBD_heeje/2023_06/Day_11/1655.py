3a8a5179d430eb8294e6485d60f8cdf537d3a76e
HeeJeYang <ghjadmb21@gmail.com>
2023-06-27 01:13:37 +0900

    1655번 문제 해결

--- DBD_heeje/2023_06/Day_11/1655.py
+++ DBD_heeje/2023_06/Day_11/1655.py
@@ -1,5 +1,31 @@
 # [BOJ] 1655. 가운데를 말해요
-# 풀이 시간: 00 분
-# 실행 시간: 00 ms
-# 메모리: 00 KB
+# 풀이 시간: 90 분
+# 실행 시간: 216 ms
+# 메모리: 43476 KB
 # 모르겠으면 검색해보고 이해해주세요!
+
+import heapq, sys
+input = sys.stdin.readline
+
+N = int(input())
+left_heap = []
+right_heap = []
+answer = []
+for i in range(N):
+    num = int(input())
+    if i % 2:
+        heapq.heappush(right_heap, num)
+    else:
+        heapq.heappush(left_heap, -num)
+    
+    if right_heap and -left_heap[0] > right_heap[0]:
+        max_left_heap = heapq.heappop(left_heap)
+        max_right_heap = heapq.heappop(right_heap)
+
+        heapq.heappush(left_heap, -max_right_heap)
+        heapq.heappush(right_heap, -max_left_heap)
+
+    answer.append(-left_heap[0])
+
+print(*answer, sep="\n")
+    
