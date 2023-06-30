f0e0de553328c554443a25199ea2b615179a2c39
희진 <109948739+lmghee@users.noreply.github.com>
2023-06-30 23:13:00 +0900

    Update 1655.java

--- DBD_heejin/2023_06/Day_11/1655.java
+++ DBD_heejin/2023_06/Day_11/1655.java
@@ -3,3 +3,22 @@
 // 실행 시간: 0 ms
 // 메모리: 0 KB
 // 모르겠으면 검색해보고 이해해주세요!
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int arr[][] = new int[20][20];
+
+        for (int i = 1; i < 20; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < 20; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
