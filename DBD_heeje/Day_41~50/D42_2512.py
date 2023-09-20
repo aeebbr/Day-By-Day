d04f9be0a2d9c087945573a36574d25c9a40a6fe
HeeJeYang <ghjadmb21@gmail.com>
2023-09-21 08:27:37 +0900

    2512번 문제 해결

--- DBD_heeje/Day_41~50/D42_2512.py
+++ DBD_heeje/Day_41~50/D42_2512.py
@@ -8,22 +8,17 @@ N = int(input())
 costs = list(map(int, input().split()))
 max_cost = int(input())
 
-start = 1
-end = 10 ** 9
+start = 0
+end = max(costs)
 
-if sum(costs) < max_cost:
-    print(max(costs))
-else:
-    while start <= end:
-        mid = (start + end) // 2
-        total_cost = 0
-        for cost in costs:
-            total_cost += cost if cost <= mid else mid
-        if max_cost == total_cost:
-            break
-        elif max_cost < total_cost:
-            end = mid - 1
-        else:
-            start = mid + 1
+while start <= end:
+    mid = (start + end) // 2
+    total_cost = 0
+    for cost in costs:
+        total_cost += cost if cost <= mid else mid
+    if max_cost < total_cost:
+        end = mid - 1
+    else:
+        start = mid + 1
 
-    print(end)
+print(end)
