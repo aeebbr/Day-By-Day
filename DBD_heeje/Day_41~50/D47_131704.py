c9f00968b1b84e8f262cabb78688728bc38ff1a6
HeeJeYang <ghjadmb21@gmail.com>
2023-09-25 19:54:07 +0900

    131704번 문제 해결

--- DBD_heeje/Day_41~50/D47_131704.py
+++ DBD_heeje/Day_41~50/D47_131704.py
@@ -1,2 +1,20 @@
 # [Programmers] 131704. 택배상자
 # 풀이 시간 : 00 분
+
+def solution(order):
+    stack = []
+    answer = 0
+    idx = 1
+
+    for o in order:
+        if o >= idx:
+            stack.extend(range(idx, o))
+            idx = o + 1
+            answer += 1
+        else:
+            if stack[-1] == o:
+                stack.pop()
+                answer += 1
+            else:
+                break
+    return answer
